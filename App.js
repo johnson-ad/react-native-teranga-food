b03430ed75c1d8e93e431c5500d62e2a7ed61fe8
='Johnson-ad <=>
2022-06-19 16:25:21 +0000

    Revision sur les scrollView FlatList useState TextInput

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import  React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList} from 'react-native';
 import Home from './screens/Home';
  
 
@@ -24,17 +24,39 @@ export default function App() {
       age: person.age + 1
     })};
 
+    const [state, setState] = useState([ 
+      {name : "Paris", key :1},
+      {name : "London", key :2},
+      {name : "New York", key :3},
+      {name : "Tokyo", key :4},
+      {name : "Paris", key :5},
+      {name : "London", key :6},
+      {name : "New York", key :7},
+      {name : "Tokyo", key :8}
+    ]);
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" /> 
-      <Text>My name is : {name}</Text>
-      <Text >His name is {person.name} and he have {person.age} old</Text>
-      <TextInput style={styles.input} placeholder="Enter your name" onChangeText={(text) => console.log(text)} />
-      <Button title="Change Name" onPress={() => clickHandler()} style={styles.button} />
-      <Button title="Change Age" onPress={() => handleChangeAge()} style={styles.button} />
+
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" /> 
+        <Text>My name is : {name}</Text>
+        <Text >His name is {person.name} and he have {person.age} old</Text>
+        <TextInput style={styles.input} placeholder="Enter your name" onChangeText={(text) => setName(text)} />
+        <Button title="Change Name" onPress={() => clickHandler()} style={styles.button} />
+        <Button title="Change Age" onPress={() => handleChangeAge()} style={styles.button} />
+
+        <FlatList 
+        data={state}
+        numColumns={2}
+        keyExtractor={item => item.key.toString()}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+        />
+
+      
 
 
-    </View>      
+      </View>
+    </ScrollView>     
   );
 }
 
@@ -43,12 +65,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    paddingTop: 90,
+    paddingTop: 70,
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'white'
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    color: 'white',
+    paddingHorizontal: 90
+ 
   },
   button:{
     marginBottom: 20
@@ -60,7 +82,18 @@ const styles = StyleSheet.create({
     marginTop: 10,
     width: 200,
     marginBottom: 10
+  },
+  items:{
+    margin: 10,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 20,
+    marginHorizontal:10,
+    marginTop:10,
+    lineHeight: 69,
+
   }
+
 });
 
  
